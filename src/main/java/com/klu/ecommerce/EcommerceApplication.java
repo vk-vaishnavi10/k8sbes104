399f93cd89da1df2566647430ee7f95c430e2c86
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2025-10-23 14:09:05 +0530

    changes in ecommerceapplication

--- src/main/java/com/klu/ecommerce/EcommerceApplication.java
+++ src/main/java/com/klu/ecommerce/EcommerceApplication.java
@@ -13,7 +13,7 @@ public class EcommerceApplication extends SpringBootServletInitializer {
     }
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(EcommerceApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EcommerceApplication.class);
     }
 }
